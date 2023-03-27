28183fa158759f643879cd7d4850db5fa1835141
ankit <ankit.lakkapragada@gmail.com>
2023-03-26 22:54:07 -0400

    fixing everything

--- backend/return_songs.py
+++ backend/return_songs.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def find_nonzero_indices(city):
     res = []
-    song_location = pd.read_csv('songs_locations_df.csv')
+    song_location = pd.read_csv('./songs_locations_df.csv')
     x = song_location[song_location[city] == 1]
     x = x.sort_values(by = 'Views', ascending = False)[:10]
     x = x[['Title','Artist', 'Year']]
