e0859e76337265be2b2eb4e6df6cd7f16cb8a628
ankit <ankit.lakkapragada@gmail.com>
2023-04-26 10:30:05 -0400

    file fixing

--- backend/helpers/query.py
+++ backend/helpers/query.py
@@ -11,13 +11,11 @@ import zipfile
 
 
 root_path = os.path.abspath(os.curdir)
-
 os.environ['ROOT_PATH'] = root_path
 
-print(os.environ['ROOT_PATH'])
 
 # unpickle wiki_tf_idf (vec2)
-with open(os.environ['ROOT_PATH'] + '/4300-Final-Project-2023/' + 'wiki_tf_idf.pkl', 'rb') as pickle_file:
+with open('./' + os.environ['ROOT_PATH']  + 'wiki_tf_idf.pkl', 'rb') as pickle_file:
     wiki_tfidf = pickle.load(pickle_file)
 
 # unpickle song_tf_idf (X)
