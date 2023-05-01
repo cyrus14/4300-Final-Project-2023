057251841aef99cfc441331130e956bd3560d40d
Samuel Meisner <sm2388@cornell.edu>
2023-05-01 17:49:07 -0400

    new pkls

--- backend/build_tfidf.py
+++ backend/build_tfidf.py
@@ -9,7 +9,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction import text
 from dataset import wiki_scraping
 
-my_stop_words = text.ENGLISH_STOP_WORDS.union({'city'})
+my_stop_words = text.ENGLISH_STOP_WORDS.union({'city', 'like', 'zwischen'})
 
 
 f = open('dataset/wiki_scraping/wiki_texts.json')
@@ -27,15 +27,18 @@ for row in big_df.iterrows():
         drop_rows.append(row[0])
 big_df.drop(drop_rows, inplace=True)
 
-vectorizer = TfidfVectorizer(max_df = 0.8, min_df = 10, norm='l2', 
-                             stop_words = list(my_stop_words))
-X = vectorizer.fit_transform(big_df['tknzd_lyrics'].apply(lambda x: " ".join(x)))
+vectorizer = TfidfVectorizer(max_df=0.8, min_df=10, norm='l2',
+                             stop_words=list(my_stop_words))
+X = vectorizer.fit_transform(
+    big_df['tknzd_lyrics'].apply(lambda x: " ".join(x)))
 
 with open('vectorizer.pkl', 'wb') as f:
     pickle.dump(vectorizer, f)
 
-index_to_word = {i:v for i, v in enumerate(vectorizer.get_feature_names_out())}
-word_to_index = {v:i for i, v in enumerate(vectorizer.get_feature_names_out())}
+index_to_word = {i: v for i, v in enumerate(
+    vectorizer.get_feature_names_out())}
+word_to_index = {v: i for i, v in enumerate(
+    vectorizer.get_feature_names_out())}
 
 with open('word_to_index.pkl', 'wb') as f:
     pickle.dump(word_to_index, f)
@@ -44,15 +47,15 @@ with open('index_to_word.pkl', 'wb') as f:
 with open('song_tf_idf.pkl', 'wb') as f:
     pickle.dump(X, f)
 
-song_to_index = {s:i for i, s in enumerate(big_df['title'])}
-index_to_song = {i:s for i, s in enumerate(big_df['title'])}
+song_to_index = {s: i for i, s in enumerate(big_df['title'])}
+index_to_song = {i: s for i, s in enumerate(big_df['title'])}
 
 with open('song_to_index.pkl', 'wb') as f:
     pickle.dump(song_to_index, f)
 with open('index_to_song.pkl', 'wb') as f:
     pickle.dump(index_to_song, f)
 
-### WIKI
+# WIKI
 
 wiki_corpus = []
 for ls in list(wiki_texts.values()):
@@ -62,7 +65,7 @@ vec2 = vectorizer.transform(wiki_corpus)
 vec2 = vec2.toarray()
 X = X.toarray()
 
-loc_to_index = {cty:i for i, cty in enumerate(wiki_texts.keys())}
+loc_to_index = {cty: i for i, cty in enumerate(wiki_texts.keys())}
 with open('wiki_tf_idf.pkl', 'wb') as f:
     pickle.dump(vec2, f)
 
