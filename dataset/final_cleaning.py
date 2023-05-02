6917b7335d8b5993e3407cbaf4579f20c60b7777
Samuel Meisner <sm2388@cornell.edu>
2023-05-02 02:34:18 -0400

    improved svd for better emotion matches. Removed unused files

--- dataset/final_cleaning.py
+++ dataset/final_cleaning.py
@@ -5,31 +5,32 @@ import numpy as np
 from scipy.sparse.linalg import svds
 from sklearn.preprocessing import normalize
 
-#MOVE ALL PKL FILES TO BACKEND!
+# MOVE ALL PKL FILES TO BACKEND!
 
 df = pd.read_csv('../backend/dataset/big_df_edited.csv')
 
 df = df.drop(columns=['Unnamed: 0'])
 
-df = df.loc[df['tag']!='misc']
+df = df.loc[df['tag'] != 'misc']
 
 df['emotions'] = df['emotions'].apply(ast.literal_eval)
-df['emotions'] = df['emotions'].apply(lambda x: [tup[0] for tup in x])
+if type(df['emotions'][0][0]) != str:
+    df['emotions'] = df['emotions'].apply(lambda x: [tup[0] for tup in x])
 
 df['log_views'] = np.log(df['views'] + 1)
 df['norm_views'] = df['log_views'] / max(df['log_views'])
 
 unique_tags = set([j for sub in df['emotions'].values for j in sub])
 
-with open('unique_tags.pkl', 'wb') as f:
+with open('../backend/unique_tags.pkl', 'wb') as f:
     pickle.dump(unique_tags, f)
 
-tag_to_index = {t:i for i, t in enumerate(unique_tags)}
-index_to_tag = {i:t for i, t in enumerate(unique_tags)}
+tag_to_index = {t: i for i, t in enumerate(unique_tags)}
+index_to_tag = {i: t for i, t in enumerate(unique_tags)}
 
-with open('tag_to_index.pkl', 'wb') as f:
+with open('../backend/tag_to_index.pkl', 'wb') as f:
     pickle.dump(tag_to_index, f)
-with open('index_to_tag.pkl', 'wb') as f:
+with open('../backend/index_to_tag.pkl', 'wb') as f:
     pickle.dump(index_to_tag, f)
 
 song_tag_mat = np.zeros((df.shape[0], len(unique_tags)))
@@ -40,20 +41,20 @@ for i in range(df.shape[0]):
         song_tag_mat[i, j] = 1
 
 
-N_FEATS = 10
+N_FEATS = 40
 docs_compressed, s, words_compressed = svds(song_tag_mat, k=N_FEATS)
 words_compressed = words_compressed.transpose()
-words_compressed_normed = normalize(words_compressed, axis = 1)
+words_compressed_normed = normalize(words_compressed, axis=1)
 docs_compressed_normed = normalize(docs_compressed, axis=1)
 
 
 td_matrix_np = song_tag_mat.transpose()
 td_matrix_np = normalize(td_matrix_np)
 
-with open('words_compressed.pkl', 'wb') as f:
+with open('../backend/words_compressed.pkl', 'wb') as f:
     pickle.dump(words_compressed, f)
 
-with open('docs_compressed_normed.pkl', 'wb') as f:
+with open('../backend/docs_compressed_normed.pkl', 'wb') as f:
     pickle.dump(docs_compressed_normed, f)
 
-df.to_csv('big_df_edited.csv')
+df.to_csv('../backend/big_df_edited.csv')
